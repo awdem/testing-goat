234808687aa199e909b37e1e893c925d6f7649bd
awdem <wdemontmollin@gmail.com>
2024-03-02 17:23:43 +0000

    new URL + view for adding to existing lists, refactor removes list URLS from superlists, FT passes

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from lists import views
+from django.urls import include, path
+from lists import views as list_views
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
+    path('', list_views.home_page, name='home'),
+    path('lists/', include("lists.urls")),
 ]
